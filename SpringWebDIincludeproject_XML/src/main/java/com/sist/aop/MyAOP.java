85cb38a86b73e21257508f5be17bd4af273a6788
yussng <tosem49071@gmail.com>
2023-07-31 17:03:48 +0900

    0731 맛집/명소/게시판(첨부파일)

---
+++ SpringWebDIincludeproject_XML/src/main/java/com/sist/aop/MyAOP.java
@@ -0,0 +1,10 @@
+package com.sist.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect // 메모리 할당이 안된다  // 공통모듈 (푸터에 공통적으로 뿌린다)
+@Component
+public class MyAOP {
+
+}
---
+++ SpringWebDIincludeproject_XML/src/main/java/com/sist/mapper/SeoulMapper.java
@@ -0,0 +1,29 @@
+package com.sist.mapper;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.sist.vo.*;
+public interface SeoulMapper {
+	// 명소 출력
+	@Select("SELECT no,title,poster,num "
+			+ "FROM (SELECT no,title,poster,rownum as num "
+			+ "FROM (SELECT no,title,poster "
+			+ "FROM seoul_location ORDER BY no ASC)) "
+			+ "WHERE num BETWEEN #{start} AND #{end}")
+	public List<SeoulVO> seoulLocationListData(Map map);
+	@Select("SELECT CEIL(COUNT(*)/12.0) FROM seoul_location")
+	public int seoulLocationTotalPage();
+	
+	// Nature 
+	@Select("SELECT no,title,poster,num "
+			+ "FROM (SELECT no,title,poster,rownum as num "
+			+ "FROM (SELECT no,title,poster "
+			+ "FROM seoul_nature ORDER BY no ASC)) "
+			+ "WHERE num BETWEEN #{start} AND #{end}")
+	public List<SeoulVO> seoulNatureListData(Map map);
+	@Select("SELECT CEIL(COUNT(*)/12.0) FROM seoul_nature")
+	public int seoulNatureTotalPage();
+	
+	//Shop
+}
