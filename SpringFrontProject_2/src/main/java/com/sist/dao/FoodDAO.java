9b9fc53e914570c4721b86b6a740012e34f071a7
yussng <tosem49071@gmail.com>
2023-08-08 17:58:38 +0900

    VueJS 푸드 목록/검색

--- SpringFrontProject_2/src/main/java/com/sist/dao/FoodDAO.java
+++ SpringFrontProject_2/src/main/java/com/sist/dao/FoodDAO.java
@@ -36,10 +36,22 @@ public class FoodDAO {
 	{
 		return mapper.foodDetailData(fno);
 	}
-	
+	// 검색 => Vue/React => 실시간
+	/*@Select("SELECT fno,name,poster,score,num "  
+			+ "FROM (SELECT fno,name,poster,score,rownum as num " 
+			+ "FROM (SELECT fno,name,poster,score " 
+			+ "FROM food_location ORDER BY fno ASC WHERE address LIKE '%'||#{address}||'%')) " 
+			+ "WHERE num BETWEEN #{start} AND #{end}")*/
 	public List<FoodLocationVO> foodFindData(Map map)
 	{
 		return mapper.foodFindData(map);
+	}
+	
+	/*@Select("SELECT CEIL(COUNT(*)/12.0) FROM food_location "
+			+ "WHERE address LIKE '%'||#{address}||'%'")*/
+	public int foodFindTotalPage(String address)
+	{
+		return mapper.foodFindTotalPage(address);
 	}
 	
 }
